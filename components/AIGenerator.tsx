332441eb0be0fde2bf34b0ffccfff14a8168fe8e
aditi-dheer <aditi02@bu.edu>
2025-04-25 23:21:10 -0400

    Added UI for displaying all AI generated NFTs and fixed CSS

--- components/AIGenerator.tsx
+++ components/AIGenerator.tsx
@@ -2,15 +2,25 @@
 
 import { client } from "@/app/client"
 import { useState } from "react";
-import { ConnectButton, MediaRenderer, useActiveAccount } from "thirdweb/react"
+import { ConnectButton, MediaRenderer, useActiveAccount, useReadContract } from "thirdweb/react"
+import { NFTCollection } from "./NFTCollections";
+import { getNFTs } from "thirdweb/extensions/erc721";
+import { contract } from "../utils/contracts";
 
 export const AIGenerator= () => {
     const account = useActiveAccount();
 
     const [imagePrompt, setImagePrompt] = useState("");
     const [generatedImage, setGeneratedImage] = useState("");
-    const [isGenerating, setIsGenerating] = useState("");
+    const [isGenerating, setIsGenerating] = useState(false);
     const [isMinting, setisMinting] = useState(false);
+
+    const { data: nfts, refetch } = useReadContract(
+        getNFTs,
+        {
+            contract: contract
+        }
+    )
 
     if (account) {
         return(
@@ -51,6 +61,70 @@ export const AIGenerator= () => {
                         </div>
                     )}
                 </div>
+                <div>
+                    <form>
+                        {!generatedImage || isMinting ? (
+                            <div style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center",
+                            }}>
+                                <input
+                                    type="text"
+                                    placeholder="Enter a prompt"
+                                    value={imagePrompt}
+                                    onChange={(e) => setImagePrompt(e.target.value)}
+                                    style={{
+                                        width: "300px",
+                                        height: "40px",
+                                        padding: "0 10px",
+                                        borderRadius: "5px",
+                                        border: "1px solid #777",
+                                        marginBottom: "10px",
+                                    }}
+                                />
+                                <button
+                                    type="submit"
+                                    disabled={isGenerating || isMinting || !imagePrompt}
+                                    style={{
+                                        width: "300px",
+                                        height: "40px",
+                                        backgroundColor: "#333",
+                                        color: "#fff",
+                                        borderRadius: "5px",
+                                        border: "none",
+                                        cursor: "pointer"
+                                    }}
+                                >
+                                    {
+                                        isGenerating ? "Generating..."
+                                        : isMinting ? "Minting..."
+                                        : "Generate and Mint NFT"
+                                    }
+                                </button>
+
+                            </div>
+                        ) : (
+                            <button
+                                onClick={() => setGeneratedImage("")}
+                                style={{
+                                    width: "300px",
+                                    height: "40px",
+                                    backgroundColor: "#333",
+                                    color: "#fff",
+                                    borderRadius: "5px",
+                                    border: "none",
+                                    cursor: "pointer"
+                                }}
+                            >
+                                Generate Another NFT
+                            </button>
+                        )}
+                    </form>
+                </div>
+                <NFTCollection
+                    nfts={nfts!}
+                />
             </div>
         );
     }
---
+++ components/NFTCollections.tsx
@@ -0,0 +1,50 @@
+import { client } from "@/app/client";
+import { NFT } from "thirdweb";
+import { MediaRenderer } from "thirdweb/react";
+
+type NFTCollectionProps = {
+    nfts: NFT[];
+}
+
+export const NFTCollection = ({ nfts }: NFTCollectionProps) => {
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            margin: "20px",
+        }}>
+            <h3>AI Generations:</h3>
+            <div style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                flexWrap: "wrap",
+                maxWidth: "600px"
+            }}>
+                {nfts && nfts.length > 0 ? nfts.map((nft) => (
+                    <div
+                        key={nft.id}
+                        style={{
+                            padding: "5px",
+                            width: "150px",
+                            height: "150px",
+                        }}
+                    >
+                        <MediaRenderer
+                            client={client}
+                            src={nft.metadata.image}
+                            style={{
+                                width: "100%",
+                                height: "100%",
+                                borderRadius: "6px",
+                            }}
+                        />
+                    </div>
+                )) : (
+                    <p>No NFTs yet.</p>
+                )}
+            </div>
+        </div>
+    )
+}
